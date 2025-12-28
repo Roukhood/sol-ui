00f18eb182bb2a2efe8f49486cd0eb91cf59377c
satish-solera <satishsolera54@gmail.com>
2025-12-28 23:00:42 +0530

    add: added the babel bundler

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import "./global.css"
+import { Button } from './components/ui/button';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className='bg-red-500'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button/>
     </View>
   );
 }
---
+++ components/ui/button.tsx
@@ -0,0 +1,10 @@
+import { Pressable } from "react-native";
+
+
+export function Button(){
+    return(
+        <Pressable>
+            hi
+        </Pressable>
+    )
+}
